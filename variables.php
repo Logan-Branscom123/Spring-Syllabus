30ef12b9b09e2d5030b48a8c80a4fc9ba258f7c1
Logan <lbransco@scholar.yc.edu>
2026-01-27 18:04:41 -0700

    Create variables.php

---
+++ variables.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+
+<?php
+    $firstName = "Logan";
+	$lastName  = "Branscom";
+	$address   = "1111 N Warner Dr";
+	$phone     = "623-264-9890";
+
+	echo "<p>First Name: $firstName</p>";
+	echo "<p>Last Name: $lastName</p>";
+	echo "<p>Address: $address</p>";
+	echo "<p>Phone Number: $phone</p>";
+?>
+
+</body>
+</html>
